2ebf7a050e7f2d7410b3b0e36a64b12ffa133d53
LsB.Dev <prof.luifel@gmail.com>
2023-05-01 16:48:42 -0300

    função de pagamento

--- src/controllers/pay.controllers.js
+++ src/controllers/pay.controllers.js
@@ -4,17 +4,17 @@ import dayjs from "dayjs";
 
 export async function payment(req, res) {
   const {productId, quant} = req.body
-  // const {authorization} = req.headers
-  // const token = authorization?.replace("Bearer ", "");
+  const {authorization} = req.headers
+  const token = authorization?.replace("Bearer ", "");
 
-  // if(!token) return res.sendStatus(401);
+  if(!token) return res.sendStatus(401);
   try {
-    // const session = await db.collection("sessions").findOne({token: token})
+    const session = await db.collection("sessions").findOne({token: token})
       const orderData = {
         productId: productId,
         quant: quant,
         date: dayjs().format('DD/MM/YYYY - HH:mm'),
-        // userId: session.userId
+        userId: session.userId
     }
     await db.collection("orders").insertOne(orderData)
     res.sendStatus(200)
