105ca4991873bfe229c32cac90cc8a27ff9032fd
LsB.Dev <prof.luifel@gmail.com>
2023-05-02 17:08:41 -0300

    ajustes Back payment

--- src/controllers/pay.controllers.js
+++ src/controllers/pay.controllers.js
@@ -3,22 +3,18 @@ import dayjs from "dayjs";
 
 
 export async function payment(req, res) {
-  const {name, token, id, text, quant, price, total, cep} = req.body
+  const {name, token, selected, total, cep} = req.body
 
   try {
     const session = await db.collection("sessions").findOne({token: token})
       const orderData = {
         name, 
         token,
-        id, 
-        text, 
-        quant, 
-        price, 
-        total,
-        cep,
-        date: dayjs().format('DD/MM/YYYY - HH:mm'),
-        userId: session.userId
-    }
+        selected, 
+        total, 
+        cep, 
+        date: dayjs().format('DD/MM/YYYY - HH:mm')
+      }
     await db.collection("orders").insertOne(orderData)
     res.sendStatus(200)
 
